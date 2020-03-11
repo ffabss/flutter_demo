b98eb77a22ce7f47edac3db7a341788fadf6c6db
aeckerstorfer <andieckerstorfer2002@gmail.com>
2020-03-11 19:54:12 +0100

    merged

--- lib/screens/homeSchauer.dart
+++ lib/screens/homeSchauer.dart
@@ -14,91 +14,89 @@ class _HomeSchauerState extends State<HomeSchauer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true, // this is all you need
-          title: Text("Schauer"),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        centerTitle: true, // this is all you need
+        title: Text("Schauer"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Material(
+                child: Text(
+              "Hallo, Alex",
+              textAlign: TextAlign.center,
+              textScaleFactor: 5,
+              style: TextStyle(color: Colors.black),
+            )),
+            Material(
+                child: Text(
+              "Enro 6",
+              textAlign: TextAlign.center,
+              textScaleFactor: 2,
+              style: TextStyle(color: Colors.grey),
+            )),
+            Container(
+              height: 45,
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-              Material(
-                  child: Text(
-                "Hallo, Alex",
-                textAlign: TextAlign.center,
-                textScaleFactor: 5,
-                style: TextStyle(color: Colors.black),
-              )),
-              Material(
-                  child: Text(
-                "Enro 6",
-                textAlign: TextAlign.center,
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.grey),
-              )),
-              Container(
-                height: 45,
-              ),
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Text(
-                          "Up Next",
-                          textAlign: TextAlign.left,
-                          textScaleFactor: 1.8,
-                          style: TextStyle(color: Colors.grey),
-                        )),
-                    Container(
-                        margin: const EdgeInsets.all(10.0),
-                        padding: const EdgeInsets.all(25.0),
-                        decoration: new BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(18)),
-                            border: new Border.all(color: Colors.grey)),
-                        child: Text(
-                          "10:30 - Stall rechts",
-                          textAlign: TextAlign.left,
-                          textScaleFactor: 1.8,
-                          style: TextStyle(color: Colors.grey),
-                        ))
-                  ]),
-              Flexible(
-                  child: GridView.count(
-                      crossAxisCount: 3,
-                      reverse: true,
-                      children: <Widget>[
-                    ButtonRedBorder(
-                        iconToUse: FontAwesome5Solid.pencil_ruler,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RouteScreen()),
-                          );
-                        }),
-                    ButtonRedBorder(
-                        iconToUse: FontAwesome5Solid.wifi,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => WifiScreen()),
-                          );
-                        }),
-                    ButtonRedBorder(
-                        iconToUse: FontAwesome5Solid.terminal,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SocketScreen()),
-                          );
-                        })
-                  ]))
-            ])));
+                  Container(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        "Up Next",
+                        textAlign: TextAlign.left,
+                        textScaleFactor: 1.8,
+                        style: TextStyle(color: Colors.grey),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(25.0),
+                      decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(18)),
+                          border: new Border.all(color: Colors.grey)),
+                      child: Text(
+                        "10:30 - Stall rechts",
+                        textAlign: TextAlign.left,
+                        textScaleFactor: 1.8,
+                        style: TextStyle(color: Colors.grey),
+                      ))
+                ]),
+            Flexible(
+                child: GridView
+                    .count(crossAxisCount: 3, reverse: true, children: <Widget>[
+              ButtonRedBorder(
+                  iconToUse: FontAwesome5Solid.pencil_ruler,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RouteCanvas()),
+                    );
+                  }),
+              ButtonRedBorder(
+                  iconToUse: FontAwesome5Solid.wifi,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WifiScreen()),
+                    );
+                  }),
+              ButtonRedBorder(
+                  iconToUse: FontAwesome5Solid.terminal,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SocketScreen()),
+                    );
+                  })
+            ]))
+          ],
+        ),
+      ),
+    );
   }
 }
